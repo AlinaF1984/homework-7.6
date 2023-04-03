8a1d9ff8fb808e8f16b97c5b3831849dcf7b4277
AlinaF1984 <afassahova9@gmail.com>
2023-04-03 16:05:29 +0500

    initial committed
    Initial commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,80 @@
+const bookOne = {
+  title: "Гарри Поттер и философский камень",
+  description: "Фантастика",
+  author: "Дж. К. Роулинг",
+};
+const bookTwo = {
+  title: "Гарри Поттер и Дары Смерти",
+  description: "Фантастика",
+  author: "Дж. К. Роулинг",
+};
+const bookThree = {
+  title: "Война и мир",
+  description: "Роман",
+  author: "Л.Н.Толстой",
+};
+
+describe("login screen", () => {
+  it("successfully logins with the valid credentials", () => {
+    cy.visit("/");
+
+    cy.login("bropet@mail.ru", "123");
+
+    cy.contains("Добро пожаловать bropet@mail.ru").should("be.visible");
+  });
+
+  it("show error message on empty login", () => {
+    cy.visit("/");
+
+    cy.login(null, "123");
+
+    cy.get("#mail")
+      .then((element) => element[0].checkValidity())
+      .should("be.false");
+
+    cy.get("#mail")
+      .then((element) => element[0].validationMessage)
+      .should("contain", "Заполните это поле");
+  });
+
+  it("show error message on empty password", () => {
+    cy.visit("/");
+
+    cy.login("bropet@mail.ru", null);
+
+    cy.get("#pass")
+      .then((element) => element[0].checkValidity())
+      .should("be.false");
+
+    cy.get("#pass")
+      .then((element) => element[0].validationMessage)
+      .should("contain", "Заполните это поле");
+  });
+});
+
+describe("Tests to check the functionality of working with books in favorites", () => {
+  beforeEach(() => {
+    cy.viewport(Cypress.env("viewportWidth"), Cypress.env("viewportHeight"));
+    cy.visit("/");
+    cy.login("bropet@mail.ru", "123");
+  });
+
+  it("add to favorites", () => {
+    cy.contains("Add to favorite").click();
+    cy.contains("Delete from favorite").should("be.visible");
+  });
+
+  it("remove from favorites", () => {
+    cy.contains("Delete from favorite").click();
+    cy.contains("Add to favorite").should("be.visible");
+  });
+
+  it("Should remove all favorite books", () => {
+    cy.addBook(bookOne );
+    cy.addBook(bookTwo);
+    cy.removeAllFavorite();
+    cy.contains("Please add some book to favorit on home page!").should(
+      "exist"
+    );
+  });
+});
